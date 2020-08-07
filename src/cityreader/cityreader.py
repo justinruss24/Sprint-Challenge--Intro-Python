f94bbb1f3124c88c9f7f36cb025278ac1713f4c9
Justin Russell <Justin@Justins-MacBook.local>
2020-08-07 08:55:09 -0700

    stretch attempt

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -25,13 +25,13 @@ def cityreader(cities=[]):
   # Ensure that the lat and lon valuse are all floats
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-  with open("cities.csv") as cities_csv:
+  with open("src/cityreader/cities.csv") as cities_csv:
     for line in cities_csv:
       split_line = line.split(",")
       if split_line[0] != "city":
         cities.append(City(split_line[0], float(split_line[3]), float(split_line[4])))
     
-    return cities
+  return cities
 
 cityreader(cities)
 
@@ -70,11 +70,26 @@ for c in cities:
 
 # TODO Get latitude and longitude values from the user
 
+
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
   # within will hold the cities that fall within the specified region
   within = []
-  
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
 
+  # TODO Ensure that the lat and lon valuse are all floats
+  # Go through each city and check to see if it falls within
+  # the specified coordinates.
+  # line1 = input("Enter lat1, lon1: ").split(",")
+  # line2 = input("Enter lat2, lon2: ").split(",")
+  # lat1, lon1, lat2, lon2 = float(line1[0]), float(line1[1]), float(line2[0]), float(line2[1])
+  if lat1 > lat2:
+    lat_a, lat_b = lat1, lat2
+  else:
+    lat_a, lat_b = lat2, lat1
+  if lon1 > lon2:
+    lon_a, lon_b = lon1, lon2
+  else:
+    lon_a, lon_b = lon2, lon1
+  for city in cities:
+    if lat_b <= city.lat <= lat_a and lon_b <= city.lon <= lon_a:
+      within.append(city)
   return within
